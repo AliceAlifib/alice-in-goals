dbaa7c8d0c62a2fda2a194f7d810a9149f31d194
Fly Dev <dev@fly.io>
2026-01-12 23:57:42 +0000

    create User schema file

---
+++ lib/alice_in_goals/accounts/user.ex
@@ -0,0 +1,24 @@
+defmodule AliceInGoals.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :google_id, :string
+    field :goals, {:array, :string}, default: []
+    field :tools, :map, default: %{}
+    field :onboarding_completed, :boolean, default: false
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :name, :google_id, :goals, :tools, :onboarding_completed])
+    |> validate_required([:email, :google_id])
+    |> unique_constraint(:email)
+    |> unique_constraint(:google_id)
+  end
+end
