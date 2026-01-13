12dc562d03f712020d7bec4e3ae270cffb8f5f15
Fly Dev <dev@fly.io>
2026-01-13 23:11:06 +0000

    add bldg-server fields to user schema

--- lib/alice_in_goals/accounts/user.ex
+++ lib/alice_in_goals/accounts/user.ex
@@ -10,6 +10,10 @@ defmodule AliceInGoals.Accounts.User do
     field :tools, :map, default: %{}
     field :onboarding_completed, :boolean, default: false
 
+    field :resident_id, :string
+    field :home_bldg_address, :string
+    field :avatar, :string
+
     timestamps()
   end
 
