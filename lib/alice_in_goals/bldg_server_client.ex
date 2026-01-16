7e92373ee23e11977b25e7488c2c952cf50ec7da
Fly Dev <dev@fly.io>
2026-01-16 02:42:28 +0000

    add detailed logging for home_bldg response structure

--- lib/alice_in_goals/bldg_server_client.ex
+++ lib/alice_in_goals/bldg_server_client.ex
@@ -14,13 +14,16 @@ defmodule AliceInGoals.BldgServerClient do
   """
   def provision_user(user) do
     with {:ok, home_bldg} <- create_home_bldg(user),
-         {:ok, resident} <- create_resident(user, home_bldg) do
-      {:ok, %{resident_id: resident["id"], home_bldg_address: home_bldg["address"]}}
-    else
-      {:error, reason} ->
-        Logger.error("Failed to provision user #{user.id} on bldg-server: #{inspect(reason)}")
-        {:error, reason}
-    end
+         _ = Logger.info("Home building response structure: #{inspect(home_bldg)}")
+
+    {:ok, resident} <-
+      create_resident(user, home_bldg) do
+        {:ok, %{resident_id: resident["id"], home_bldg_address: home_bldg["address"]}}
+      else
+        {:error, reason} ->
+          Logger.error("Failed to provision user #{user.id} on bldg-server: #{inspect(reason)}")
+          {:error, reason}
+      end
   end
 
   @doc """
